f2dbeac003dec4a1e03b8f7fd89dc530f4a93bc9
Muhammad Khizar <90056057+mhmkhizar@users.noreply.github.com>
2025-08-16 22:43:59 +0500

    feat: update hero section styling and content structure for improved presentation

--- src/home/home.js
+++ src/home/home.js
@@ -1,1 +1,25 @@
+import { createElement } from "../index.js";
 import "./home.css";
+
+const content = {
+  heading: `Welcome to The Flavor Haven`,
+  subHeading: `Where Every Bite is an Experience`,
+  text: `Step into a world of flavors crafted with passion and precision. At
+            The Flavor Haven, we believe that dining is more than just
+            eating—it's about savoring every detail, from the first sip to the
+            last bite. Enjoy a cozy, elegant atmosphere paired with dishes
+            inspired by global cuisines.`,
+};
+
+const section = createElement(`section`, `hero-section`);
+const h2 = createElement(`h2`, `heading`);
+const h3 = createElement(`h3`, `sub-heading`);
+const p = createElement(`p`, `text`);
+
+h2.textContent = content.heading;
+h3.textContent = content.subHeading;
+p.textContent = content.text;
+
+section.append(h2, h3, p);
+
+export { section };
--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import "./styles.css";
-import "./home/home.js";
+import { section } from "./home/home.js";
 
 console.log(`hello`);
+const contentContainer = document.querySelector(`#content`);
+contentContainer.appendChild(section);
+
+function createElement(name, className, id) {
+  if (!name) return;
+  const element = document.createElement(`${name}`);
+  if (className) element.classList.add(`${className}`);
+  if (id) element.id = `${id}`;
+  return element;
+}
+
+export { createElement };
