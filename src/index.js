4400583db34b4611b31f8d5a489412f83eec2ded
Muhammad Khizar <90056057+mhmkhizar@users.noreply.github.com>
2025-08-18 16:22:19 +0500

    feat: add about page content and styles, including contact info and opening hours

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import "./styles.css";
 import { loadHome } from "./home/home.js";
 import { loadMenu } from "./menu/menu.js";
+import { loadAbout } from "./about/about.js";
 
 // loadHome();
 // loadMenu();
+// loadAbout();
