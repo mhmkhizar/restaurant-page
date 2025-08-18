c8b7a26469dcdbb7d2df9308af1928d46ba1b8d6
Muhammad Khizar <90056057+mhmkhizar@users.noreply.github.com>
2025-08-18 17:16:45 +0500

    feat: refactor import paths and enhance button event listeners for improved functionality

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import "./styles.css";
-import { loadHome } from "./home/home.js";
-import { loadMenu } from "./menu/menu.js";
-import { loadAbout } from "./about/about.js";
+import { loadHome } from "./home.js";
+import { loadMenu } from "./menu.js";
+import { loadAbout } from "./about.js";
 
-// loadHome();
-// loadMenu();
-// loadAbout();
+const contentContainer = document.querySelector(`#content`);
+
+const homeButton = document.querySelector(`#homeButton`);
+const menuButton = document.querySelector(`#menuButton`);
+const aboutButton = document.querySelector(`#aboutButton`);
+
+homeButton.addEventListener(`click`, (e) => {
+  contentContainer.innerHTML = ``;
+  loadHome();
+});
+
+menuButton.addEventListener(`click`, (e) => {
+  contentContainer.innerHTML = ``;
+  loadMenu();
+});
+
+aboutButton.addEventListener(`click`, (e) => {
+  contentContainer.innerHTML = ``;
+  loadAbout();
+});
+
+loadHome();
