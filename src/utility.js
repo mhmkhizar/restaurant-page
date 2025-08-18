df1c0a9af6d5b1992bf721c055185425d7f30a7b
Muhammad Khizar <90056057+mhmkhizar@users.noreply.github.com>
2025-08-18 17:15:34 +0500

    feat: remove unused CSS and JavaScript files for about, home, menu, and utility components

--- src/utility.js
+++
@@ -1,9 +0,0 @@
-function createElement(name, className, id) {
-  if (!name) return;
-  const element = document.createElement(`${name}`);
-  if (className) element.classList.add(`${className}`);
-  if (id) element.id = `${id}`;
-  return element;
-}
-
-export { createElement };
